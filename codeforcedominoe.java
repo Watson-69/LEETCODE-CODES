f8d1d33e50d29b307c22cc5ac510fe68f008d4b8
Watson-69 <morsecode453@gmail.com>
2026-08-19 11:14:38 +0530

    Codeforce problem 50A done

---
+++ codeforcedominoe.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class codeforcedominoe {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int m,n;
+        m=sc.nextInt();
+        n=sc.nextInt();
+        int res=(m*n)/2;
+        System.out.println(res);
+    }
+}
