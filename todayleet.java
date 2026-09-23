b50256b00853c445aa39fff8620d953b3b502858
Watson-69 <morsecode453@gmail.com>
2026-09-23 14:22:21 +0530

    APPRAOCH 2 of thsi

--- todayleet.java
+++ todayleet.java
@@ -33,4 +33,33 @@ class Solution {
         return maxLength == -1 ? -1 : nums.length - maxLength;
     }
 } // approach 1
+
+
+class Solution {
+    public int minOperations(int[] nums, int x) {
+        int result = chut_ke_lode(nums, 0, nums.length - 1, x);
+        return result == Integer.MAX_VALUE ? -1 : result;
+    }
+
+    private int chut_ke_lode(int[] nums, int left, int right, int remainingX) {
+        if (remainingX == 0) {
+            return 0;
+        }
+        if (remainingX < 0 || left > right) {
+            return Integer.MAX_VALUE;
+        }
+
+        int removeLeft = chut_ke_lode(nums, left + 1, right, remainingX - nums[left]);
+        if (removeLeft != Integer.MAX_VALUE) {
+            removeLeft += 1;
+        }
+
+        int removeRight = chut_ke_lode(nums, left, right - 1, remainingX - nums[right]);
+        if (removeRight != Integer.MAX_VALUE) {
+            removeRight += 1;
+        }
+
+        return Math.min(removeLeft, removeRight);
+    }
+} // a pproach 2 recursion not opitmixaable
 }
