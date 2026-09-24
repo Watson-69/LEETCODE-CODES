0ec0bc52606d9b85b966432caac5a92139040466
Watson-69 <morsecode453@gmail.com>
2026-09-24 11:31:30 +0530

    TODAY LEETCODE QUESTION

---
+++ questionoftheday.java
@@ -0,0 +1,23 @@
+public class questionoftheday {
+    class Solution {
+    public int smallestIndex(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            int n = nums[i];
+            int digitSum = 0;
+            
+       wo sawal
+            while (n > 0) {
+                digitSum += n % 10;
+                n /= 10;
+            }
+        
+            conditosn
+            if (digitSum == i) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
+
+}
